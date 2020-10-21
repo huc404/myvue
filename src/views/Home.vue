af0a77bf1568ac538d9c74c2fe69364f7aafd4ad
kirs <chenwei@163.com>
2020-10-21 10:42:28 +0800

    整理的项目模板

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+const num = 0
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {}
 }
 </script>
